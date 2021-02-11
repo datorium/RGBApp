1d2187f476cd85131ee698b8923e845c326ce795
Datorium <hello@datorium.lv>
2021-02-11 20:00:44 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RGBApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColorBox = new System.Windows.Forms.PictureBox();
+            this.RedBox = new System.Windows.Forms.PictureBox();
+            this.NumericRed = new System.Windows.Forms.NumericUpDown();
+            this.GreenBox = new System.Windows.Forms.PictureBox();
+            this.BlueBox = new System.Windows.Forms.PictureBox();
+            this.NumericGreen = new System.Windows.Forms.NumericUpDown();
+            this.NumericBlue = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ColorBox
+            // 
+            this.ColorBox.BackColor = System.Drawing.Color.Red;
+            this.ColorBox.Location = new System.Drawing.Point(878, 291);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(264, 240);
+            this.ColorBox.TabIndex = 0;
+            this.ColorBox.TabStop = false;
+            this.ColorBox.Click += new System.EventHandler(this.ColorBox_Click);
+            this.ColorBox.MouseEnter += new System.EventHandler(this.ColorBox_MouseEnter);
+            this.ColorBox.MouseLeave += new System.EventHandler(this.ColorBox_MouseLeave);
+            // 
+            // RedBox
+            // 
+            this.RedBox.BackColor = System.Drawing.Color.Red;
+            this.RedBox.Location = new System.Drawing.Point(49, 104);
+            this.RedBox.Name = "RedBox";
+            this.RedBox.Size = new System.Drawing.Size(205, 193);
+            this.RedBox.TabIndex = 1;
+            this.RedBox.TabStop = false;
+            // 
+            // NumericRed
+            // 
+            this.NumericRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericRed.Location = new System.Drawing.Point(49, 43);
+            this.NumericRed.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.NumericRed.Name = "NumericRed";
+            this.NumericRed.Size = new System.Drawing.Size(205, 41);
+            this.NumericRed.TabIndex = 2;
+            this.NumericRed.ValueChanged += new System.EventHandler(this.NumericRed_ValueChanged);
+            // 
+            // GreenBox
+            // 
+            this.GreenBox.BackColor = System.Drawing.Color.Lime;
+            this.GreenBox.Location = new System.Drawing.Point(283, 104);
+            this.GreenBox.Name = "GreenBox";
+            this.GreenBox.Size = new System.Drawing.Size(205, 193);
+            this.GreenBox.TabIndex = 3;
+            this.GreenBox.TabStop = false;
+            // 
+            // BlueBox
+            // 
+            this.BlueBox.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BlueBox.Location = new System.Drawing.Point(522, 104);
+            this.BlueBox.Name = "BlueBox";
+            this.BlueBox.Size = new System.Drawing.Size(205, 193);
+            this.BlueBox.TabIndex = 4;
+            this.BlueBox.TabStop = false;
+            // 
+            // NumericGreen
+            // 
+            this.NumericGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericGreen.Location = new System.Drawing.Point(283, 43);
+            this.NumericGreen.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.NumericGreen.Name = "NumericGreen";
+            this.NumericGreen.Size = new System.Drawing.Size(205, 41);
+            this.NumericGreen.TabIndex = 5;
+            this.NumericGreen.ValueChanged += new System.EventHandler(this.NumericGreen_ValueChanged);
+            // 
+            // NumericBlue
+            // 
+            this.NumericBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumericBlue.Location = new System.Drawing.Point(522, 43);
+            this.NumericBlue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.NumericBlue.Name = "NumericBlue";
+            this.NumericBlue.Size = new System.Drawing.Size(205, 41);
+            this.NumericBlue.TabIndex = 6;
+            this.NumericBlue.ValueChanged += new System.EventHandler(this.NumericBlue_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 556);
+            this.Controls.Add(this.NumericBlue);
+            this.Controls.Add(this.NumericGreen);
+            this.Controls.Add(this.BlueBox);
+            this.Controls.Add(this.GreenBox);
+            this.Controls.Add(this.NumericRed);
+            this.Controls.Add(this.RedBox);
+            this.Controls.Add(this.ColorBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericBlue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ColorBox;
+        private System.Windows.Forms.PictureBox RedBox;
+        private System.Windows.Forms.NumericUpDown NumericRed;
+        private System.Windows.Forms.PictureBox GreenBox;
+        private System.Windows.Forms.PictureBox BlueBox;
+        private System.Windows.Forms.NumericUpDown NumericGreen;
+        private System.Windows.Forms.NumericUpDown NumericBlue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGBApp
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorBox_Click(object sender, EventArgs e)
+        {
+            int red = rand.Next(0, 256);
+            int green = rand.Next(0, 256);
+            int blue = rand.Next(0, 256);
+
+            ColorBox.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void ColorBox_MouseEnter(object sender, EventArgs e)
+        {
+            ColorBox.BackColor = Color.Black;
+        }
+
+        private void ColorBox_MouseLeave(object sender, EventArgs e)
+        {
+            ColorBox.BackColor = Color.Red;
+        }
+
+        private void NumericRed_ValueChanged(object sender, EventArgs e)
+        {
+            //pixel bulb can have value between 0-255
+            int red = (int)NumericRed.Value;
+            RedBox.BackColor = Color.FromArgb(red, 0, 0);
+        }
+
+        private void NumericGreen_ValueChanged(object sender, EventArgs e)
+        {
+            int green = (int)NumericGreen.Value;
+            GreenBox.BackColor = Color.FromArgb(0, green, 0);
+        }
+
+        private void NumericBlue_ValueChanged(object sender, EventArgs e)
+        {
+            int blue = (int)NumericBlue.Value;
+            BlueBox.BackColor = Color.FromArgb(0, 0, blue);
+        }
+    }
+}
